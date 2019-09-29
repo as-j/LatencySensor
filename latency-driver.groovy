f65db355aa6471e37360e520202154ada8fca6e3
Andrew Stanley-Jones <asj@cban.com>
2019-09-29 13:24:10 -0700

    Use a TimedSession sensor type

--- latency-driver.groovy
+++ latency-driver.groovy
@@ -6,65 +6,74 @@
 metadata {
     definition (name: "Latency Sensor", namespace: "asj", author: "asj") {
         capability "Switch"
-        capability "PressureMeasurement"
-        capability "Sensor"
+        capability "TimedSession"
+        attribute  "timeElapsed", "number"
     }
 
     preferences {
-        input name: "autoOff", type: "bool", title: "Auto Off Timing", defaultValue: true
-        input name: "autoOffTimeMs", type: "number", title: "Auto Off Time (ms)", defaultValue: 50, required: true
-        input name: "tick", type: "bool", title: "Send Periodic Tick", defaultValue: false
-        input name: "tickTimeMs", type: "number", title: "Time of Tick (ms)", defaultValue: 1000, required: true
+        input name: "autoStop", type: "bool", title: "Auto Stop", defaultValue: true
+        input name: "autoStopTimeMs", type: "number", title: "Auto Stop Time (ms)", defaultValue: 50, required: true
+        input name: "ignoreStart", type: "bool", title: "Ignore Start When Already Running", defaultValue: false
         //standard logging options
         input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: false
         input name: "txtEnable", type: "bool", title: "Enable descriptionText logging", defaultValue: false
     }
 }
 
-def logsOff(){
+def logsStop(){
     log.warn "debug logging disabled..."
     device.updateSetting("logEnable",[value:"false",type:"bool"])
 }
 
 //capability and device methods
-def off() {
-    if (device.currentValue("switch") == "off") return
+def stop() {
+    if (device.currentValue("sessionStatus") != "running") return
 
     state.endTime = now()
-    state.latency = state.endTime - state.startTime
+    state.timeElapsed = state.endTime - state.startTime
 
-    unschedule()
-
-    if (logEnable) log.info "${device.displayName} On to off time took: ${state.latency}"
-    def name = "pressure"
-    def unit = "ms"
-    def descriptionText = "${device.displayName} is ${state.latency} ms"
-    sendEvent(name: "pressure",value: state.latency, descriptionText: txtEnable ? descriptionText : "",unit: unit)
-    sendEvent(name: "switch", value: "off", isStateChange: true)
+    if (logEnable) log.info "${device.displayName} time elapsed ${state.timeElapsed}"
+    sendEvent(name: "timeElapsed",
+              value: state.timeElapsed,
+              descriptionText: "${device.displayName} timeElapsed: ${state.timeElapsed} ms",
+              unit: "ms")
+    sendEvent(name: "sessionStatus", value: "stop")
 }
 
-def on() {
-    unschedule()
-    sendEvent(name: "switch", value: "on", isStateChange: true)
+def start() {
+    if (ignoreStart && (device.currentValue("sessionStatus") == "running")) return
+    sendEvent(name: "sessionStatus", value: "running")
     state.startTime = now()
-    if (autoOff) runInMillis(autoOffTimeMs, off)
-    if (tick) {
-        def date = new Date()
-        for (def offset = tickTimeMs; offset < autoOffTimeMs; offset += tickTimeMs) {
-            date.setTime(state.startTime + offset)
-            schedule(date, tick, [overwrite: false])
-        }
+    if (autoStop) runInMillis(autoStopTimeMs, stop)
+}
+
+def cancel() {
+    sendEvent(name: "sessionStatus", value: "cancel")
+}
+
+def pause() {
+    if (logEnable) log.info "${device.displayName} doesn't support pause"
+}
+
+def setTimeRemaining(timeRemaining) {
+    if (timeRemaining) {
+        device.updateSetting("autoStopTimeMs",[type:"number", value:timeRemaining])
+        device.updateSetting("autoStop",[type:"bool", value: true])
+        if (logEnable) log.info "${device.displayName} Enable and set autoStopTimeMs to ${autoStopTimeMs}"
+    } else {
+        device.updateSetting("autoStop",[type:"bool", value: false])
+        if (logEnable) log.info "${device.displayName} Disabled autoStop"
     }
 }
 
-def tick() {
-    if (device.currentValue("switch") == "off") return
+// Implement switch on/off so we can use the device in RM4
+def on() {
+    sendEvent(name: "switch", value: "on")
+    start()
+}
 
-    def latency = now() - state.startTime
-    if (logEnable) log.info "${device.displayName} Tick at: ${latency}"
-    def name = "pressure"
-    def unit = "ms"
-    def descriptionText = "${device.displayName} Tick at ${latency} ms"
-    sendEvent(name: "pressure",value: latency, descriptionText: txtEnable ? descriptionText : "",unit: unit)
+def off() {
+    stop()
+    sendEvent(name: "switch", value: "off")
 }
 
